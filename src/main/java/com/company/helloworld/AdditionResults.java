6ce0e54ce9058ec70687dddaa6ae391431bab70b
testuser <testuser@gmail.com>
2021-05-31 06:32:51 +0000

    Third parameter inserted

--- src/main/java/com/company/helloworld/AdditionResults.java
+++ src/main/java/com/company/helloworld/AdditionResults.java
@@ -3,8 +3,9 @@ public class AdditionResults {
     int num1 = Integer.parseInt(args[0]);
     int num2 = Integer.parseInt(args[1]);
     int num3 = Integer.parseInt(args[2]);
+    int num4 = Integer.parseInt(args[3]);
 
-    Addition myAddition = new Addition(num1,num2,num3);
+    Addition myAddition = new Addition(num1,num2,num3,num4);
     
     String output = String.format("The sum is %d", myAddition.getSum());
 
